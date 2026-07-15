ec5691de5cb1e0d7d1c2137045624c7884b97c68
sheikh hamza <sheikhhamza1501@gmail.com>
2026-07-16 01:37:38 +0530

    reset default details

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://interviewai-backend-bof4.onrender.com/api",
+    baseURL: "http://localhost:8080/api",
     headers: {
         "Content-Type": "application/json",
     },
